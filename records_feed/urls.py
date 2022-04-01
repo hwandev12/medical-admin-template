177649b16977d509dd2fe79518aee433cb9c915c
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-01 15:45:15 +0500

    adding urls

--- records_feed/urls.py
+++ records_feed/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import *
 
 urlpatterns = [
-    path('', home)
+    path('', feedback_users, name='customers'),
+    path('<pk>/', select_info)
 ]
--- records_feed/views.py
+++ records_feed/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from . import models
+from django.shortcuts import get_object_or_404
 
-def home(request):
-    return render(request, 'base.html')
+
+def feedback_users(request):
+    setters = models.Setter.objects.all()
+    context = {"setters": setters}
+    return render(request, 'feedback_users.html', context)
+
+
+def select_info(request, pk):
+    feedback = get_object_or_404(models.Setter, id=pk)
+    return render(request, 'details/feedback_details.html')
