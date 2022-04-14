da76c76b8f5f730daabbafca4878e4e27caa5df1
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-14 21:46:46 +0500

    password reset view

--- back_medical/urls.py
+++ back_medical/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import(
+     LoginView,
+     LogoutView,
+     PasswordResetView,
+)
 from records_feed.views import HomeView, RegisterView
 
 urlpatterns = [
@@ -9,6 +13,7 @@ urlpatterns = [
     path('signup/', RegisterView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('password-reset-view/', PasswordResetView.as_view(), name='password_reset_view'),
     path('', include('records_feed.urls')),
     path('agents/', include('agents.urls', namespace='agents'))
 ]
