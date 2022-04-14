4d02eda12f560b514477023e173b157ef28d417d
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-14 19:53:10 +0500

    adding email backend

--- back_medical/settings.py
+++ back_medical/settings.py
@@ -119,8 +119,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 AUTH_USER_MODEL = 'records_feed.User'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
