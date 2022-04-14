b8152312aab423633f22497b637ddd96e654ed22
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-14 19:49:55 +0500

    create agent view

--- agents/forms.py
+++ agents/forms.py
@@ -1,10 +1,17 @@
 from dataclasses import fields
 from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
 from records_feed.models import Agent
+
+User = get_user_model()
 
 class AgentCreateModel(forms.ModelForm):
     class Meta:
-        model = Agent
+        model = User
         fields = (
-            'user',
+            'email',
+            'username',
+            'first_name',
+            'last_name'
         )
