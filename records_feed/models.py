4d92e2346c88d77268cdbe14352ece532b70e3f6
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-21 12:07:15 +0500

    add category model

--- records_feed/models.py
+++ records_feed/models.py
@@ -2,42 +2,61 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
     is_organiser = models.BooleanField(default=True)
     is_agent = models.BooleanField(default=False)
 
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    
+
     def __str__(self):
         return str(self.user.username)
 
+
 class Setter(models.Model):
-    
+
     class Meta:
         verbose_name = "Setter"
         verbose_name_plural = "Feedback users"
-        
+
     name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField()
     message = models.CharField(max_length=500)
     contact_number = models.CharField(max_length=30)
     organiser = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    agent = models.ForeignKey("Agent", null=True, blank=True, on_delete=models.SET_NULL)
+    agent = models.ForeignKey(
+        "Agent", null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(
+        'Category', blank=True, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.name
-    
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'My Category Lists'
+    category_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return str(self.category_name)
+
+
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     organiser = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    
+
     def __str__(self):
         return str(self.user)
-    
+
+
 def create_post_save(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
+
 
 post_save.connect(create_post_save, sender=User)
