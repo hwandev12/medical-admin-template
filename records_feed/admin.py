ebc16d96da6211c66c5759c14f6f65d8e8b4bb69
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-21 12:07:49 +0500

    add category to admin

--- records_feed/admin.py
+++ records_feed/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Setter, User, Agent, UserProfile
+from .models import Setter, User, Agent, UserProfile, Category
 
 admin.site.register(Setter)
 admin.site.register(User)
 admin.site.register(Agent)
 admin.site.register(UserProfile)
+admin.site.register(Category)
